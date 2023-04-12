6385332c9e2e91da3014467095af3e1eed2e4e4d
Andre Pereira <andreserafim11@gmail.com>
2023-04-12 14:41:18 -0300

    aside-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import './global.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import styles from './App.module.css';
+import { SideBar } from './components/SideBar';
 
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="André Pereira"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quod soluta molestiae eveniet expedita obcaecati sequi alias, laboriosam blanditiis quibusdam, natus in minus quas! Laudantium adipisci pariatur magnam consequatur deserunt."
+          />
+          <Post
+            author=""
+            content=" Excepturi quod soluta molestiae eveniet expedita obcaecati sequi alias, laboriosam blanditiis quibusdam, natus in minus quas! Laudantium adipisci pariatur magnam consequatur deserunt."
+          />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
-
-export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './SideBar.module.css';
+
+export function SideBar(props) {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/88911949?v=4"
+        />
+        <strong>André Pereira</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
